7b8761a1b1129e7d183fe097933b64cddb3604d3
physicalrobot <kimboho@gmail.com>
2025-01-15 14:30:34 -0800

    trying a different workflow added system/hypr.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,54 +1,42 @@
-{description = "A NixOS Configuration for tars";
+{
+  description = "Modular NixOS Configuration with Separate Home Manager";
 
-inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs";
-  home-manager = {
-    url = "github:nix-community/home-manager";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-  textfox.url = "github:adriankarlen/textfox";
-};
-
-outputs = inputs@{ self, nixpkgs, home-manager, textfox, ... }: let
-  system = "x86_64-linux"; # Replace with your actual architecture
-  pkgs = nixpkgs.legacyPackages.${system};
-in {
-  nixosConfigurations.tars = nixpkgs.lib.nixosSystem {
-    system = system;
-    specialArgs = { inherit inputs system; };
-    modules = [
-      ./configuration.nix
-      ./hardware-configuration.nix
-      home-manager.nixosModules.home-manager
-      {
-        # Enable home-manager integration
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        
-        # Define user home-manager configuration
-        home-manager.users.viku = {
-          imports = [
-            ./home.nix
-            inputs.textfox.homeManagerModules.default
-          ];
-        };
-      }
-    ];
-  };
-
-  devShell = {
-    x86_64-linux = pkgs.mkShell {
-      packages = with pkgs; [
-        node2nix
-        nodejs
-        pnpm
-        yarn
-        pkgs.nodePackages.prettier
-        git
-        typos
-      ];
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-};
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      tars = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+
+    homeConfigurations = {
+      viku = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        modules = [ ./home.nix ];
+      };
+    };
+
+    devShell = {
+      x86_64-linux = nixpkgs.legacyPackages."x86_64-linux".mkShell {
+        packages = with nixpkgs.legacyPackages."x86_64-linux"; [
+          node2nix
+          nodejs
+          pnpm
+          yarn
+          pkgs.nodePackages.prettier
+          git
+          typos
+        ];
+      };
+    };
+  };
 }
 
---
+++ nixos/system/hypr.nix
@@ -0,0 +1,76 @@
+{ inputs, config, pkgs, ... }:
+
+{
+  # Enable Cachix for Hyprland binaries
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
+  # Enable Hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  # Environment variables for Wayland support
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1"; # Hint for Electron apps to use Wayland
+  };
+
+  # Install required packages
+  environment.systemPackages = with pkgs; [
+    # Terminals
+    kitty
+    foot
+
+    # File manager (KDE Dolphin)
+    kdePackages.dolphin
+    kdePackages.dolphin-plugins
+    kdePackages.ark
+    kdePackages.konsole
+    kdePackages.kio-admin
+    kdePackages.breeze-icons
+    kdePackages.kwrite
+
+    # Icon themes
+    dracula-icon-theme
+    nordzy-icon-theme
+    epapirus-icon-theme
+    morewaita-icon-theme
+    la-capitaine-icon-theme
+    gnome-icon-theme
+
+    # Hyprland utilities
+    waybar                 # Status bar
+    swaynotificationcenter # Notifications
+    hyprpaper              # Wallpaper
+    rofi-wayland           # Application launcher
+    grim                   # Screenshots
+    slurp                  # Screenshot region selector
+    hyprshot               # Hyprland-specific screenshots
+    cliphist               # Clipboard manager
+    wl-clipboard           # Clipboard utilities
+    wlogout                # Logout menu
+
+    # GTK and QT Themes
+    magnetic-catppuccin-gtk
+    tokyonight-gtk-theme
+    catppuccin-kvantum
+    nwg-look               # Setup GTK themes
+    libsForQt5.qt5ct
+    libsForQt5.qtstyleplugin-kvantum
+    kdePackages.qt6ct
+    qt5.qtwayland
+    qt6.qtwayland
+
+    # Utilities
+    pavucontrol            # Audio control
+    polkit_gnome           # Polkit authentication
+    hyprpicker             # Color picker
+    wtype                  # Typing automation
+    font-manager           # Font management
+    xdg-desktop-portal-hyprland # XDG Portal
+  ];
+}
+
